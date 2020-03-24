cc4930e2708d99dd234460f2e59a26a5a4489093
Patxi Crabiffosse <pcrabiffosse@gmail.com>
2020-03-24 14:17:35 +0100

    fixed imports

--- tests/json/test_utils.py
+++ tests/json/test_utils.py
@@ -1,4 +1,4 @@
-from jsonize.json.utils import get_item_from_json_path, write_item_in_path, JSONPath
+from jsonize.utils.json import get_item_from_json_path, write_item_in_path, JSONPath
 from copy import deepcopy
 import unittest
 
