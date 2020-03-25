f930e1fef1a04a3db0f2c7e7787d9392ab09a2c7
FJ Crabiffosse <fjcrabiffosse@bmsgroup.io>
2020-03-25 16:33:34 +0100

    completed map parsing from a JSON input

--- jsonize/map_parser.py
+++ jsonize/map_parser.py
@@ -1,11 +1,57 @@
 from json import load
+from jsonize.utils.json import JSONNode, JSONNodeType
+from jsonize.utils.xml import XMLNodeType, XMLNode
+from jsonize.xml_to_json.base import XMLNodeToJSONNode, Transformation
+from jsonschema import Draft7Validator
 from pathlib import Path
-from typing import Dict
+from typing import Dict, List, Optional
 
-def parse(map: Path) -> Dict:
-    parsed_json_map = load(map)
-    return parsed_json_map
 
-with Path('./schema/sample.json').open() as map_file:
-    result = parse(map_file)
-    print()
+def parse_node_map(node_map: Dict, transformations: List[Transformation]) -> XMLNodeToJSONNode:
+    """
+    Parses a serialized JSON (Python dictionary) defining a Jsonize node map and creates
+    its corresponding XMLNodeToJSONNode mapping.
+    :param node_map: A dictionary containing the serialized representation of a JSON NodeMap as specified in Jsonize Schema.
+    :param transformations: A list of Transformation from which the
+    :return:
+    """
+    from_xml_node = XMLNode(node_map['from']['path'], XMLNodeType[node_map['from']['type']])
+    to_json_node = JSONNode(node_map['to']['path'], JSONNodeType[node_map['to']['type']])
+    try:
+        unparsed_item_mappings = node_map['itemMappings']
+        item_mappings = [parse_node_map(item_mapping, transformations) for item_mapping in unparsed_item_mappings]  # type: Optional[List[XMLNodeToJSONNode]]
+    except KeyError:
+        item_mappings = None
+    try:
+        transformation_name = node_map['transformation']  # type: Optional[str]
+    except KeyError:
+        transformation_name = ''
+
+    if transformation_name:
+        try:
+            transformation = [transf for transf in transformations if transf.name == transformation_name][0]
+        except IndexError:
+            raise ValueError(f'The transformation with name "{transformation_name}" cannot be found in the "transformations" parameter.')
+    else:
+        transformation = None
+
+    parsed_node_map = XMLNodeToJSONNode(from_xml_node=from_xml_node,
+                                        to_json_node=to_json_node,
+                                        item_mappings=item_mappings,
+                                        transform=transformation)
+    return parsed_node_map
+
+
+def parse(jsonize_map: List[Dict], transformations: Optional[List[Transformation]]=None) -> List[XMLNodeToJSONNode]:
+    """
+    Produces a list of XMLNodeToJSONNode mappings from the Python serialization of a Jsonize map.
+    A Jsonize map is defined as an instance of jsonize-map.schema.json.
+    :param jsonize_map: A Python serialized Jsonize map, for example, the output of json.load function when given the
+    a Jsonize map defined in JSON.
+    :param transformations: A list of Transformation that are used in the jsonize map.
+    :return: A list of XMLNodeToJSONNode that defines the XML to JSON mapping.
+    """
+    if transformations is None:
+        transformations = []
+    node_mappings = [parse_node_map(mapping, transformations) for mapping in jsonize_map]
+    return node_mappings
