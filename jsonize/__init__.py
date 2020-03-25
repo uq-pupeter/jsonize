3166faa2c9d03306e3b3a3d21b48d88a2ee4274b
FJ Crabiffosse <fjcrabiffosse@bmsgroup.io>
2020-03-25 17:17:38 +0100

    simplify package structure

--- jsonize/__init__.py
+++ jsonize/__init__.py
@@ -1,3 +1,3 @@
 from .utils.json import JSONNodeType, JSONNode, JSONPath
 from .utils.xml import XPath, XMLNodeType, XMLNode
-from .xml_to_json.base import xml_document_to_json, XMLNodeToJSONNode, Transformation
+from .utils.mapping import Transformation, XMLNodeToJSONNode, xml_document_to_json
