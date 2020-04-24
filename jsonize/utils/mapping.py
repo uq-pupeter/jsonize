179319fc213cd0c5340e1ece52219a31c4184d8c
FJ Crabiffosse <fjcrabiffosse@bmsgroup.io>
2020-04-24 16:01:35 +0200

    added function to infer jsonize maps from XML input file

--- jsonize/utils/mapping.py
+++ jsonize/utils/mapping.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
-from json import load
+from json import load, dump
 from pathlib import Path
 from xml.etree import ElementTree as etree
-from jsonize.utils.xml_utils import XMLNode, XMLNodeType, XPath
+from jsonize.utils.xml_utils import XMLNode, XMLNodeType, build_node_tree
 from jsonize.utils.json_utils import JSONNode, JSONNodeType, JSONPath, write_item_in_path
 from typing import Dict, List, Optional, Callable, Iterable
 
@@ -224,3 +224,14 @@ def xml_document_to_json(xml_document: Path,
     for mapping in jsonize_map:
         result = mapping.map(xml_etree, result, xml_namespaces=xml_namespaces)
     return result
+
+
+def infer_jsonize_map(xml_document: Path, output_map: Path, xml_namespaces: Dict = None, attribute_tag: str = '', keep_namespaces: bool = True) -> None:
+    xml_etree = etree.parse(str(xml_document))
+    node_tree = build_node_tree(xml_etree, xml_namespaces=xml_namespaces)
+    if keep_namespaces:
+        jsonized = node_tree.to_jsonize(attributes=attribute_tag, namespaces='preserve')
+    else:
+        jsonized = node_tree.to_jsonize(attributes=attribute_tag, namespaces='ignore')
+    with output_map.open('w') as output_file:
+        dump(jsonized, output_file)
