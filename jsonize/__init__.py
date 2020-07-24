1d8cbba3ed1aa61373bea59c569c92d8b07bfd9e
FJ Crabiffosse <fjcrabiffosse@bmsgroup.io>
2020-07-24 15:20:48 +0200

    added import of xml_document_to_json_document

--- jsonize/__init__.py
+++ jsonize/__init__.py
@@ -1,3 +1,3 @@
 from .utils.json_utils import JSONNodeType, JSONNode, JSONPath
 from .utils.xml_utils import XPath, XMLNodeType, XMLNode
-from .utils.mapping import Transformation, XMLNodeToJSONNode, xml_document_to_dict, infer_jsonize_map
+from .utils.mapping import Transformation, XMLNodeToJSONNode, xml_document_to_dict, infer_jsonize_map, xml_document_to_json_document
