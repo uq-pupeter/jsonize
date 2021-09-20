e403657c962a7617e1887a9a43cb75f2f8c6ccd7
FJ Crabiffosse <fjcrabiffosse@bmsgroup.io>
2021-09-20 16:05:15 +0200

    added functions to __init__ import

--- jsonize/__init__.py
+++ jsonize/__init__.py
@@ -1,3 +1,11 @@
 from .utils.json import JSONNodeType, JSONNode, JSONPath, infer_json_type
 from .utils.xml import XPath, XMLNodeType, XMLNode
-from .utils.mapping import Transformation, XMLNodeToJSONNode, xml_document_to_dict, infer_jsonize_map, xml_document_to_json_document
+from .utils.mapping import (Transformation,
+                            XMLNodeToJSONNode,
+                            xml_document_to_dict,
+                            infer_jsonize_map,
+                            infer_path_type,
+                            xml_document_to_json_document,
+                            json_document_to_json_document,
+                            json_document_to_dict,
+                            JSONNodeToJSONNode)
